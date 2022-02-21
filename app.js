47c439f269cbc21ce33926a43e906b56fd290894
sotorodrigo <mdqrodrigosoto@gmail.com>
2022-02-21 00:10:04 -0300

    finalizado el home, agregando login

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 
+app.get('/register', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
 
 
 
