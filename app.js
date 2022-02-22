27782be83bc1627b64d5561c349307ecd958bf86
sotorodrigo <mdqrodrigosoto@gmail.com>
2022-02-21 23:04:52 -0300

    mas formularios

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.get('/register', (req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
 
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
 
 
 
